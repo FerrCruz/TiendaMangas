dafd10a62902b9c073c9ead9c8729ba508353bbc
FerrCruz <cfer3890@gmail.com>
2020-03-27 05:07:24 -0300

    Correcion de codigo

---
+++ bd/mangaBD.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS VentaMangas;
+CREATE DATABASE VentaMangas;
+USE VentaMangas;
+
+CREATE TABLE Mangas(
+	MangaID int AUTO_INCREMENT,
+	Nombre varchar(50),
+	Autor varchar(50),
+	Tomo varchar(20),
+	Precio float,
+	Imagen VARCHAR(500),
+	PRIMARY KEY(MangaID)
+);
+
+INSERT INTO Mangas VALUES
+("","Kimetsu No Yaiba","Koyoharu Gotōge","18",385,"img/kny18.jpg"),
+("","Kimetsu No Yaiba","Koyoharu Gotōge","1",385,"img/kny1.jpg"),
+("","Fairy Tail","Hiro Mashima","4",385,"img/fairytail.jpg"),
+("","One Punch Man","One","21",385,"img/opm21.jpg"),
+("","Boku No Hero Academia","Kōhei Horikoshi","18",385,"img/myhero18.jpg"),
+("","Shingeki No Kyojin","Hajime Isayama","30",385,"img/snk30.jpg"),
+("","Shingeki No Kyojin","Hajime Isayama","29",385,"img/snk29.jpg"),
+("","Shingeki No Kyojin","Hajime Isayama","28",385,"img/snk28.jpg"),
+("","Shingeki No Kyojin","Hajime Isayama","27",385,"img/snk27.jpg"),
+("","Go-toubun No Hanayome","Negi Haruba","12",385,"img/gnh12.jpg"),
+("","Tokyo Ghoul","Sui Ishida","1",385,"img/tokyoghoul1.jpg"),
+("","Jojos","Hirohiko Araki","2",385,"img/jojos2.jpg");
+
+CREATE TABLE Cuentas(
+	CuentaID int AUTO_INCREMENT,
+	usuario varchar(50),
+	contrasenia varchar(50),
+	primary key(CuentaID)
+);
+
+INSERT INTO Cuentas VALUES
+("","admin","admin"),
+("","fernando","123");
---
+++ js/mangas.js
@@ -0,0 +1,139 @@
+var articulos =
+      {"datos" : [
+        {
+            "Titulo" : "Kimetsu No Yaiba",
+            "Autor" : "Koyoharu Gotoge",
+            "Tomo" : "18",
+            "Precio" : "$385",
+            "img" : ["img/kny18.jpg"]
+        },
+        {
+            "Titulo" : "Kimetsu No Yaiba",
+            "Autor" : "Koyoharu Gotoge",
+            "Tomo" : "1",
+            "Precio" : "$385",
+            "img" : ["img/kny1.jpg"]
+        },
+        {
+            "Titulo" : "Fairy Tail",
+            "Autor" : "Hiro Mashima",
+            "Tomo" : "4",
+            "Precio" : "$385",
+            "img" : ["img/fairytail.jpg"]
+        },
+        {
+            "Titulo" : "One Punch Man",
+            "Autor" : "One",
+            "Tomo" : "21",
+            "Precio" : "$385",
+            "img" : ["img/opm21.jpg"]
+        },
+        {
+            "Titulo" : "Boku No Hero Academia",
+            "Autor" : "Kohei Horikoshi",
+            "Tomo" : "18",
+            "Precio" : "$385",
+            "img" : ["img/myhero18.jpg"]
+        },
+        {
+            "Titulo" : "Shingeki No Kyojin",
+            "Autor" : "Hajime Isayama",
+            "Tomo" : "30",
+            "Precio" : "$385",
+            "img" : ["img/snk30.jpg"]
+        },
+        {
+            "Titulo" : "Shingeki No Kyojin",
+            "Autor" : "Hajime Isayama",
+            "Tomo" : "29",
+            "Precio" : "$385",
+            "img" : ["img/snk29.jpg"]
+        },
+        {
+            "Titulo" : "Shingeki No Kyojin",
+            "Autor" : "Hajime Isayama",
+            "Tomo" : "28",
+            "Precio" : "$385",
+            "img" : ["img/snk28.jpg"]
+        },
+        {
+            "Titulo" : "Shingeki No Kyojin",
+            "Autor" : "Hajime Isayama",
+            "Tomo" : "27",
+            "Precio" : "$385",
+            "img" : ["img/snk27.jpg"]
+        },
+        {
+            "Titulo" : "Go-toubun No Hanayome",
+            "Autor" : "Negi Haruba",
+            "Tomo" : "12",
+            "Precio" : "$385",
+            "img" : ["img/gnh12.jpg"]
+        },
+        {
+            "Titulo" : "Tokyo Ghoul",
+            "Autor" : "Sui Ishida",
+            "Tomo" : "1",
+            "Precio" : "$385",
+            "img" : ["img/tokyoghoul1.jpg"]
+        },
+        {
+            "Titulo" : "Jojos",
+            "Autor" : "Hirohiko Araki",
+            "Tomo" : "2",
+            "Precio" : "$385",
+            "img" : ["img/jojos2.jpg"]
+        }
+    ]
+};
+    /*{"datos" : [
+        {
+            "Titulo" : "Kimetsu No Yaiba",
+            "img" : ["img/kny18.jpg"]
+        },
+        {
+            "Titulo" : "Kimetsu No Yaiba",
+            "img" : ["img/kny1.jpg"]
+        },
+          {
+            "Titulo" : "Fairy Tail",
+            "img" : ["img/fairytail.jpg"]
+        },
+          {
+            "Titulo" : "One Punch Man",
+            "img" : ["img/opm21.jpg"]
+        },
+          {
+            "Titulo" : "Boku No Hero Academia",
+            "img" : ["img/myhero18.jpg"]
+        },
+          {
+            "Titulo" : "Shingeki No Kyojin",
+            "img" : ["img/snk30.jpg"]
+        },
+          {
+            "Titulo" : "Shingeki No Kyojin",
+            "img" : ["img/snk29.jpg"]
+        },
+          {
+            "Titulo" : "Shingeki No Kyojin",
+            "img" : ["img/snk28.jpg"]
+        },
+          {
+            "Titulo" : "Shingeki No Kyojin",
+            "img" : ["img/snk27.jpg"]
+        },
+          {
+            "Titulo" : "Go-toubun No Hanayome",
+            "img" : ["img/gnh12.jpg"]
+        },
+          {
+            "Titulo" : "Tokyo Ghoul",
+            "img" : ["img/tokyoghoul1.jpg"]
+        },
+          {
+            "Titulo" : "Jojos",
+            "img" : ["img/jojos2.jpg"]
+        }
+    ]
+}*/
